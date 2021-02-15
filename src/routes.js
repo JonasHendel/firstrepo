b03aec99683cca69bbabcf230fbfc21ad01890bf
JonasHendel <oslo.jonas@gmail.com>
2021-02-15 19:46:47 +0100

    Added Rejection and Approval

--- src/routes.js
+++ src/routes.js
@@ -6,6 +6,9 @@ const UserController = require("./controllers/UserController");
 const EventContoller = require("./controllers/EventController");
 const DashboardController = require("./controllers/DashboardController");
 const LoginController = require("./controllers/LoginController");
+const RegistrationController = require("./controllers/RegistrationController");
+const ApprovalController = require("./controllers/ApprovalController")
+const RejectionController = require("./controllers/RejectionController")
 
 const uploadConfig = require("./config/upload");
 const upload = multer(uploadConfig);
@@ -14,15 +17,23 @@ routes.get("/status", (req, res) => {
 	res.send({ status: 200 });
 });
 
-// TODO SubscribeController
-// TODO ApprovalController
-// TODO RejectionConttoller
 
+
+
+// Registration
+routes.post("/registration/:eventId", RegistrationController.create);
+routes.get("/registration/:registration_id", RegistrationController.getRegistration);
+routes.post("/registration/:registration_id/approvals", ApprovalController.approval)
+routes.post("/registration/:registration_id/rejections", RejectionController.rejection)
+
+
+
+// Login
 routes.post("/login", LoginController.store);
 
 // Dashboard
-routes.get("/events/:sport", DashboardController.getAllEvents);
-routes.get("/events", DashboardController.getAllEvents);
+routes.get("/dashboard/:sport", DashboardController.getAllEvents);
+routes.get("/dashboard", DashboardController.getAllEvents);
 routes.get("/event/:eventId", DashboardController.getEventById);
 
 // Event
